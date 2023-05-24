627053b24e707ffb6d190fa883b5a0fef7c28a43
Anderson Mena <amserna@stefanini.com>
2023-05-23 22:23:48 -0500

    Actualización conexión db

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 const connection = async () => {
-    const db = 'mi_red_social';
+    const db = 'mi-red-social';
     try {
-        await mongoose.connect(`mongodb://127.0.0.1:27017/${db}`);
+        // Local
+        // await mongoose.connect(`mongodb://127.0.0.1:27017/${db}`);
+        // prd
+        await mongoose.connect(`mongodb+srv://andersonmena87:%5Cw.0R%3B%5Bs&C,%7D*GC'~,vf@cluster0.uduo29b.mongodb.net/${db}`);
         console.log(`Conectado correctamente a ${db}`);
     } catch (error) {
         console.log(error);
